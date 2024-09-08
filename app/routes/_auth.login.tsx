96d78e6af9623bcf9aad0b40191be3b0ce45e4f9
Kouhei <{mkkhiti.iltill@gmail.com}>
2024-09-09 01:20:07 +0900

    handle login error

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,18 +1,23 @@
 import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import { json, useActionData, useNavigation } from "@remix-run/react";
+import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { auth } from "~/lib/firebase/firebase";
 
 export default function Login() {
-  //  const nav = useNavigate();
+  const actionData = useActionData<typeof action>();
+  const navigation = useNavigation();
+  console.log(navigation.state);
   return (
     <>
       <h1>ログインして♡</h1>
       <form method="post" className="flex flex-col gap-3 mt-6">
         <Input type="email" name="email" placeholder="email" />
         <Input type="password" name="password" placeholder="password" />
-        <Button>ログイン</Button>
+        <Button disabled={navigation.state !== "idle"}>ログイン</Button>
+        <p className="text-red-600 text-sm">{actionData?.error}</p>
       </form>
     </>
   );
@@ -27,7 +32,20 @@ export async function action({ request }: ActionFunctionArgs) {
     throw new Error("Please enter email and password");
   }
 
-  await signInWithEmailAndPassword(auth, email, password);
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      console.error({ code: error.code });
+      if (["auth/wrong-password", "auth/user-not-found"].includes(error.code)) {
+        return json(
+          { error: "メールアドレスまたはパスワードが間違っています" },
+          { status: 400 }
+        );
+      }
+      return json({ error: "何かしらのエラーが起きました" }, { status: 400 });
+    }
+  }
 
   return redirect("/");
 }
