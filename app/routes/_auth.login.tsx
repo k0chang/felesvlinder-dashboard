e2559d43fbbc95de385420859ef84818206bb1e6
Kouhei <{mkkhiti.iltill@gmail.com}>
2024-09-09 01:37:33 +0900

    remix is god

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -1,5 +1,5 @@
-import { ActionFunctionArgs, redirect } from "@remix-run/node";
-import { json, useActionData, useNavigation } from "@remix-run/react";
+import { ActionFunctionArgs, redirect, TypedResponse } from "@remix-run/node";
+import { Form, json, useActionData, useNavigation } from "@remix-run/react";
 import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Button } from "~/components/ui/button";
@@ -9,27 +9,39 @@ import { auth } from "~/lib/firebase/firebase";
 export default function Login() {
   const actionData = useActionData<typeof action>();
   const navigation = useNavigation();
-  console.log(navigation.state);
   return (
     <>
       <h1>ログインして♡</h1>
-      <form method="post" className="flex flex-col gap-3 mt-6">
+      <Form method="post" className="flex flex-col gap-3 mt-6">
         <Input type="email" name="email" placeholder="email" />
+        <p className="text-red-600 text-sm">{actionData?.errors.email}</p>
         <Input type="password" name="password" placeholder="password" />
+        <p className="text-red-600 text-sm">{actionData?.errors.password}</p>
         <Button disabled={navigation.state !== "idle"}>ログイン</Button>
-        <p className="text-red-600 text-sm">{actionData?.error}</p>
-      </form>
+        <p className="text-red-600 text-sm">{actionData?.errors.root}</p>
+      </Form>
     </>
   );
 }
 
-export async function action({ request }: ActionFunctionArgs) {
+export async function action({ request }: ActionFunctionArgs): Promise<
+  TypedResponse<{
+    errors: {
+      root?: string;
+      email?: string;
+      password?: string;
+    };
+  }>
+> {
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
 
-  if (typeof email !== "string" || typeof password !== "string") {
-    throw new Error("Please enter email and password");
+  if (typeof email !== "string" || !email.length) {
+    return json({ errors: { email: "メールアドレスを入力" } }, { status: 400 });
+  }
+  if (typeof password !== "string" || !password.length) {
+    return json({ errors: { password: "パスワードを入力" } }, { status: 400 });
   }
 
   try {
@@ -39,11 +51,16 @@ export async function action({ request }: ActionFunctionArgs) {
       console.error({ code: error.code });
       if (["auth/wrong-password", "auth/user-not-found"].includes(error.code)) {
         return json(
-          { error: "メールアドレスまたはパスワードが間違っています" },
+          {
+            errors: { root: "メールアドレスまたはパスワードが間違っています" },
+          },
           { status: 400 }
         );
       }
-      return json({ error: "何かしらのエラーが起きました" }, { status: 400 });
+      return json(
+        { errors: { root: "何かしらのエラーが起きました" } },
+        { status: 400 }
+      );
     }
   }
 
