8fd26e4c01e10f32fd100d6d13d6f3b52fc710c8
Kouhei <{mkkhiti.iltill@gmail.com}>
2024-09-30 18:11:49 +0900

    strokeProps

--- app/components/editor/icon.tsx
+++ app/components/editor/icon.tsx
@@ -18,19 +18,43 @@ import { CustomElement } from "~/types/editor";
 export const Icon = forwardRef<
   HTMLSpanElement,
   ComponentProps<"span"> & { type: CustomElement["type"] | "unlink" }
->(({ type, className, ...props }, ref) => (
-  <span {...props} ref={ref} className={cn(className, "align-text-bottom")}>
-    {type === "bold" && <Bold className={className} />}
-    {type === "italic" && <Italic className={className} />}
-    {type === "underline" && <Underline className={className} />}
-    {type === "link" && <Link />}
-    {type === "code" && <Code className={className} />}
-    {type === "heading-1" && <Heading1 className={className} />}
-    {type === "heading-2" && <Heading2 className={className} />}
-    {type === "heading-3" && <Heading3 className={className} />}
-    {type === "numbered-list" && <ListOrdered className={className} />}
-    {type === "bulleted-list" && <List className={className} />}
-    {type === "unlink" && <Unlink className={className} />}
-  </span>
-));
+>(({ type, className, ...props }, ref) => {
+  const strokeProps = {
+    strokeLinecap: "square" as const,
+    strokeLinejoin: "miter" as const,
+  };
+  return (
+    <span {...props} ref={ref} className={cn(className, "align-text-bottom")}>
+      {type === "bold" && <Bold {...strokeProps} className={className} />}
+      {type === "italic" && <Italic {...strokeProps} className={className} />}
+      {type === "underline" && (
+        <Underline
+          {...strokeProps}
+          strokeLinejoin="miter"
+          className={className}
+        />
+      )}
+      {type === "link" && <Link {...strokeProps} />}
+      {type === "code" && (
+        <Code {...strokeProps} strokeLinejoin="miter" className={className} />
+      )}
+      {type === "heading-1" && (
+        <Heading1 {...strokeProps} className={className} />
+      )}
+      {type === "heading-2" && (
+        <Heading2 {...strokeProps} className={className} />
+      )}
+      {type === "heading-3" && (
+        <Heading3 {...strokeProps} className={className} />
+      )}
+      {type === "numbered-list" && (
+        <ListOrdered {...strokeProps} className={className} />
+      )}
+      {type === "bulleted-list" && (
+        <List {...strokeProps} className={className} />
+      )}
+      {type === "unlink" && <Unlink {...strokeProps} className={className} />}
+    </span>
+  );
+});
 Icon.displayName = "Icon";
